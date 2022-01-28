d67e305131fe7fd2cedaf85e09b5fee198ed365f
vahgrig <vahgrig@gmail.com>
2022-01-28 23:49:45 +0400

    md5sum chek@ chi stacvum menak

--- backup.sh
+++ backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-
+#checking arguments
 if [ $# -eq 0 ] 
 then 
 	echo
@@ -16,7 +15,7 @@ else
 fi
 if [[ -d $dest ]]
 then
-	echo "ok"
+	echo 
 else 
 	echo "directory not exist, do you want to create it yes/no"
 	read answer
@@ -27,25 +26,35 @@ else
 		exit 1
 	fi
 fi
-
 backup_files=$1
-
-
-
+#geting curent last created file name
+cd $dest
+last_file_name="$(ls -tr | tail -n 1)"
+#creating file name
 #time="$(date +"%Y-%m %d")"
 filename=$(basename $backup_files)
-archive_file="$filename-$(date +"%Y-%m-%d").tgz"
-
-
-echo "Backing up $backup_files to $dest/$archive_file"
-
-
-tar czf $dest/$archive_file $backup_files
-
+archive_file="$filename-$(date +"%Y-%m-%d-%H-%M-%S").tgz"
+gzip -r -c  $backup_files>$dest/$archive_file
+curent_created_file="$(ls -tr | tail -n 1)"
 echo "backup is finished"
-
-
-echo "#######################END########################################"
 ls -lh $dest
-
+#checking buckup file modifed or not 
+if [ $(ls | wc -l) -gt 1 ]
+then
+	if [ "$(curl --silent $last_file_name | md5sum)" = "$(curl --silent $curent_created_file | md5sum)" ]
+	then 
+		echo "deleting file"
+		rm -f $curent_created_file
+	else
+		echo "modified file"
+	fi
+#deleting older files except last 5 created files 
+	if [ $(ls | wc -l) -gt 1 ]
+	then
+		while [ $(ls | wc -l) -gt 5 ]
+		do 
+			rm -f $(ls -t | tail -1)
+		done
+	fi
+fi
 #find $dest -type f -mtime +5 -delete
